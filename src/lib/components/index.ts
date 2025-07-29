32c8ec2eee68ba0da2270842ddaef88197d2589a
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-07-29 14:47:43 -0300

    Project structure

---
+++ src/lib/components/index.ts
@@ -0,0 +1,5 @@
+export { default as Header } from "./Header.svelte";
+export { default as Carroussel } from "./Carroussel.svelte";
+export { default as ColorBand } from "./ColorBand.svelte";
+export { default as Logo } from "./Logo.svelte";
+export * from "./io";
---
+++ src/lib/sections/index.ts
@@ -0,0 +1,4 @@
+export { default as AboutSection } from "./AboutSection.svelte";
+export { default as ActivitiesSection } from "./ActivitiesSection.svelte";
+export { default as ContactSection } from "./ContactSection.svelte";
+export { default as MainSection } from "./MainSection.svelte";
