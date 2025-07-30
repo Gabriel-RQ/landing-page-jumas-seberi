ea265313ac72cf0bef1d90ed1002f8784b85d1fb
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-07-30 10:18:42 -0300

    feat: adds CTA and IconButton components

---
+++ src/lib/components/io/CTA.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  type CTAProps = {
+    text: string;
+    href: string;
+    class?: string;
+  };
+
+  let { text, href, class: className }: CTAProps = $props();
+</script>
+
+<a
+  {href}
+  class="rounded-full bg-jumas-yellow text-jumas-blue font-semibold text-3xl flex content-center size-fit px-2 py-3 {className}"
+>
+  {text}
+</a>
--- src/lib/components/io/index.ts
+++ src/lib/components/io/index.ts
@@ -1,6 +1,5 @@
 export { default as Button } from "./Button.svelte";
 export { default as CTA } from "./CTA.svelte";
-export { default as IconButton } from "./IconButton.svelte";
 export { default as Input } from "./Input.svelte";
 export { default as Link } from "./Link.svelte";
 export { default as TextArea } from "./TextArea.svelte";
