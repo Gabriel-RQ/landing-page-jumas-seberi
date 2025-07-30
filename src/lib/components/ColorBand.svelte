a5cee0f886921688229c0d4ec0fc1162145f2228
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-07-30 10:17:25 -0300

    feat: adds ColorBand component

---
+++ src/lib/components/ColorBand.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  const colorClasses = [
+    "bg-jumas-blue",
+    "bg-jumas-red",
+    "bg-jumas-green",
+    "bg-jumas-yellow",
+  ];
+</script>
+
+<div class="absolute inset-x-0 top-0 inline-flex h-1.5 *:grow">
+  {#each colorClasses as className}
+    <span class={className}></span>
+  {/each}
+</div>
