e069d843e56c1a55326f7b6bdfe9a9004253f2a6
Gabriel-RQ <123425138+Gabriel-RQ@users.noreply.github.com>
2025-07-30 10:17:38 -0300

    feat: adds Logo component

---
+++ src/lib/components/Logo.svelte
@@ -0,0 +1,4 @@
+<div class="flex items-center gap-4 size-fit">
+  <img src="/logo.webp" alt="Logo do Jumas Brasil" class="h-20" />
+  <h1 class="font-bold text-2xl font-montserrat">Jumas Seberi</h1>
+</div>
